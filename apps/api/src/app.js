5df7466627764c3d7c559facc550e92c953d1157
Adrian <adrian@institchu.com>
2020-11-05 16:16:37 +1100

    setup inital api app

---
+++ apps/api/src/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+/**
+ * CONFIGS
+ */
+
+// use helmet for setting HTTP headers
+const helmet = require("helmet");
+app.use(helmet());
+
+// use compression for performance optimisation
+const compression = require("compression");
+app.use(compression());
+
+// use body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json({ type: "application/json" }));
+
+// use morgan
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+// config cors
+const cors = require("cors");
+const allowedCors = ["http://localhost:3000"];
+app.use(cors({ origin: allowedCors }));
+
+/**
+ * ROUTES
+ */
+// const routes = require("./routes");
+
+/**
+ * ROUTES
+ */
+app.get("/", (req, res) => res.send(""));
+// app.use("/customers", routes.customer);
+
+module.exports = app;
---
+++ apps/api/src/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const http = require("http");
+const mongoose = require("mongoose");
+const app = require("./app");
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`connected to ${process.env.MONGO_URI}`);
+
+    const httpServer = http.createServer(app);
+
+    return httpServer.listen(8080);
+  })
+  .then(() => {
+    console.log("server started");
+  });
