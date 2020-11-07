f477f93f721faa7ef2a63d2b20c9db6005e7e724
Adrian <leonsio192@gmail.com>
2020-11-07 14:05:00 +1100

    set customer module test

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -5,7 +5,10 @@ const app = require('./app');
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log(`connected to ${process.env.MONGO_URI}`);
 
--- apps/api/tests/integration.test.js
+++ apps/api/tests/integration.test.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = require('../src/app');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const supertest = require('supertest');
-
+const faker = require('faker');
 let mongod, server, request;
 
 beforeAll(async () => {
@@ -11,7 +11,10 @@ beforeAll(async () => {
   const uri = await mongod.getUri();
 
   mongoose.Promise = global.Promise;
-  await mongoose.connect(uri, { useNewUrlParser: true });
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   server = app.listen(8080);
   request = supertest.agent(server);
@@ -25,4 +28,32 @@ afterAll(async () => {
 
 test('Test root path', (done) => {
   return request.get('/').expect(200, done);
+});
+
+describe('CUSTOMER MODULE', () => {
+  const fakeFullName = faker.name.firstName() + ' ' + faker.name.lastName();
+  const fakeEmail = faker.internet.email();
+  const fakePassword = faker.random.words(4);
+
+  test('Create Customer', (done) => {
+    return request
+      .post('/customers')
+      .send({
+        name: fakeFullName,
+        email: fakeEmail,
+        password: fakePassword,
+      })
+      .then((res) => {
+        const { success, payload } = res.body;
+
+        expect(res.statusCode).toBe(200);
+        expect(success).toBe(true);
+
+        expect(payload.name).toBe(fakeFullName);
+        expect(payload.email).toBe(fakeEmail);
+        expect(payload.password).toBe(fakePassword);
+
+        done();
+      });
+  });
 });
