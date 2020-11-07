d45f3cdaa404aba18394f762722ccde9a98c3604
Adrian <leonsio192@gmail.com>
2020-11-07 11:53:57 +1100

    set states for form

--- apps/frontend/src/Form/Form.js
+++ apps/frontend/src/Form/Form.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { useForm, Controller } from "react-hook-form";
+import React, { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 
-import { ErrorMessage } from "@hookform/error-message";
+import { ErrorMessage } from '@hookform/error-message';
 
 import {
   makeStyles,
@@ -9,16 +9,16 @@ import {
   Grid,
   TextField,
   Button,
-} from "@material-ui/core";
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   form: {
     padding: theme.spacing(2),
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    width: "100%",
-    justifyContent: "center",
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    justifyContent: 'center',
   },
   container: {
     margin: theme.spacing(2),
@@ -27,21 +27,24 @@ const useStyles = makeStyles((theme) => ({
 
 const Form = () => {
   const { handleSubmit, register, control, errors } = useForm();
+  const [error, setError] = useState(false);
+  const [confirm, setConfirm] = useState(false);
 
   const signup = (data) => {
-    return fetch("http://localhost:8080/customers/", {
-      method: "POST",
+    return fetch('http://localhost:8080/customers/', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log("Create Success: ", data);
+        console.log('Create Success: ', data);
+        !data.success ? setError(true) : setConfirm(true);
       })
       .catch((error) => {
-        console.error("ERROR: ", error);
+        setError(true);
       });
   };
 
@@ -50,95 +53,103 @@ const Form = () => {
   return (
     <Container maxWidth="sm">
       <Grid container spacing={3}>
-        <form className={classes.form} onSubmit={handleSubmit(signup)}>
-          <Grid item xs={12}>
-            <Controller
-              as={
-                <TextField
-                  className={classes.container}
-                  id="standard-basic"
-                  fullWidth
-                  type="text"
-                  ref={register}
-                  label="Full Name"
-                />
-              }
-              control={control}
-              name="name"
-              defaultValue=""
-              rules={{ required: "Name is required." }}
-            />
-            <ErrorMessage errors={errors} name="name" />
-          </Grid>
-          <Grid item xs={12}>
-            <Controller
-              as={
-                <TextField
-                  className={classes.container}
-                  id="standard-basic"
-                  fullWidth
-                  type="text"
-                  ref={register}
-                  label="Email"
-                />
-              }
-              control={control}
-              name="email"
-              defaultValue=""
-              rules={{ required: "Email is required." }}
-            />
-            <ErrorMessage errors={errors} name="email" />
-          </Grid>
-          <Grid item xs={12}>
-            <Controller
-              as={
-                <TextField
-                  className={classes.container}
-                  id="standard-basic"
-                  fullWidth
-                  type="text"
-                  ref={register}
-                  label="Password"
-                />
-              }
-              control={control}
-              name="password"
-              defaultValue=""
-              rules={{ required: "Password is required." }}
-            />
-          </Grid>
-          <ErrorMessage errors={errors} name="password" />
-          <Grid item xs={12}>
-            <Controller
-              as={
-                <TextField
-                  className={classes.container}
-                  id="standard-basic"
-                  fullWidth
-                  type="text"
-                  ref={register}
-                  label="Confirm Password"
-                />
-              }
-              control={control}
-              name="confirmPassword"
-              defaultValue=""
-              rules={{ required: "Password is required." }}
-            />
-            <ErrorMessage errors={errors} name="confirmPassword" />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              className={classes.container}
-              variant="contained"
-              fullWidth
-              color="primary"
-              type="submit"
-            >
-              Sign Up
-            </Button>
-          </Grid>
-        </form>
+        {!confirm ? (
+          <form className={classes.form} onSubmit={handleSubmit(signup)}>
+            <Grid item xs={12}>
+              <Controller
+                as={
+                  <TextField
+                    className={classes.container}
+                    id="standard-basic"
+                    fullWidth
+                    type="text"
+                    ref={register}
+                    label="Full Name"
+                  />
+                }
+                control={control}
+                name="name"
+                defaultValue=""
+                rules={{ required: 'Name is required.' }}
+              />
+              <ErrorMessage errors={errors} name="name" />
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                as={
+                  <TextField
+                    className={classes.container}
+                    id="standard-basic"
+                    fullWidth
+                    type="text"
+                    ref={register}
+                    label="Email"
+                  />
+                }
+                control={control}
+                name="email"
+                defaultValue=""
+                rules={{ required: 'Email is required.' }}
+              />
+              <ErrorMessage errors={errors} name="email" />
+              {error && <span>The email has been used already</span>}
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                as={
+                  <TextField
+                    className={classes.container}
+                    id="standard-basic"
+                    fullWidth
+                    type="text"
+                    ref={register}
+                    label="Password"
+                  />
+                }
+                control={control}
+                name="password"
+                defaultValue=""
+                rules={{ required: 'Password is required.' }}
+              />
+            </Grid>
+            <ErrorMessage errors={errors} name="password" />
+            <Grid item xs={12}>
+              <Controller
+                as={
+                  <TextField
+                    className={classes.container}
+                    id="standard-basic"
+                    fullWidth
+                    type="text"
+                    ref={register}
+                    label="Confirm Password"
+                  />
+                }
+                control={control}
+                name="confirmPassword"
+                defaultValue=""
+                rules={{ required: 'Password is required.' }}
+              />
+              <ErrorMessage errors={errors} name="confirmPassword" />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                className={classes.container}
+                variant="contained"
+                fullWidth
+                color="primary"
+                type="submit"
+              >
+                Sign Up
+              </Button>
+            </Grid>
+          </form>
+        ) : (
+          <span>
+            "Thank you for signing up to Gym Time, please check your email for
+            your activation link!"
+          </span>
+        )}
       </Grid>
     </Container>
   );
